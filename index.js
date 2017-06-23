276362f4d0ff8f995dc097270a8348cc815230ca
tjvr <tim@tjvr.org>
2017-06-23 15:36:10 +0100

    Initial!

---
+++ index.js
@@ -0,0 +1,154 @@
+
+const WebSocket = require('ws')
+const emitter = require('./emitter')
+
+
+class Sox {
+  constructor(ws) {
+    this.ws = ws
+    ws.on('message', this._onMessage.bind(this))
+    this.on('welcome', this._onWelcome.bind(this))
+    this.on('position', this._onPosition.bind(this))
+    this.on('book', this._onBook.bind(this))
+
+    // order tracking
+    this.orders = Object.create(null)
+    this.highestId = 0
+    this.on('fill', this._onFill.bind(this))
+    this.on('out', this._onOut.bind(this))
+  }
+
+  send(name, msg) {
+    let json = Object.assign({}, msg, {_type: name})
+    this.ws.send(JSON.stringify(json))
+  }
+
+  _onMessage(data) {
+    let json = JSON.parse(data)
+    let type = json._type
+    delete json._type
+    this.emit(type, json)
+  }
+
+  /* * */
+
+  _onWelcome({symbols}) {
+    this.symbols = symbols
+    this.stocks = symbols.map(s => new Stock(s, this))
+  }
+
+  _onPosition({balance, positions}) {
+    this.balance = balance
+    this.positions = positions
+  }
+
+  _onBook({symbol, buys, sells}) {
+    let stock = this.stocks.find(s => s.symbol === symbol)
+    stock.emit('book', {buys, sells})
+  }
+
+
+  /* * */
+
+  buy(symbol, price, size) { return this.order(symbol, 'buy', price, size) }
+  sell(symbol, price, size) { return this.order(symbol, 'sell', price, size) }
+
+  order(symbol, dir, price, size) {
+    let order = new Order(symbol, dir, price, size)
+    let order_id = this._register(order)
+    this.send(dir, {order_id, symbol, price, size})
+    return order
+  }
+
+  cancel(order) {
+    //delete this.orders[order._id]
+    this.send('cancel', {order_id: order._id})
+  }
+
+  _register(order) {
+    let order_id = this.highestId = (this.highestId + 1) % 0xffff
+    //if (this.orders[order_id]) throw 'oops'
+    this.orders[order_id] = order
+    console.log('in', order_id)
+    order._sox = this
+    return order._id = order_id
+  }
+
+  _onFill({order_id, price, size}) {
+    let order = this.orders[order_id]
+    console.log('fill', order_id)
+    order.emit('fill', {price, size})
+  }
+
+  _onOut({order_id}) {
+    let order = this.orders[order_id]
+    delete this.orders[order_id]
+    console.log('out', order_id)
+    order.emit('out')
+  }
+}
+emitter(Sox.prototype)
+
+
+class Stock {
+  constructor(symbol, sox) {
+    this._sox = sox
+    this.symbol = symbol
+    this.buys = []
+    this.sells = []
+
+    this.on('book', this._onBook.bind(this))
+  }
+
+  buy(price, size) { return this.order('buy', price, size) }
+  sell(price, size) { return this.order('sell', price, size) }
+  order(dir, price, size) { return this._sox.order(this.symbol, dir, price, size) }
+  
+  _onBook({buys, sells}) {
+    this.buys = buys
+    this.sells = sells
+  }
+}
+emitter(Stock.prototype)
+
+
+class Order {
+  constructor(symbol, dir, price, size) {
+    Object.assign(this, {symbol, dir, price, size})
+  }
+
+  cancel() {
+    this._sox.cancel(this)
+  }
+}
+emitter(Order.prototype)
+
+ 
+function connect(host, name, token) {
+  return new Promise((resolve, reject) => {
+    let ws = new WebSocket(`ws://${host}/ws`)
+     
+    ws.on('open', () => {
+      ws.send(JSON.stringify({_type: 'identify', name, token}))
+    })
+
+    function message(data) {
+      let json = JSON.parse(data)
+      if (json._type === 'error') reject(json.error)
+      ws.removeEventListener('message', message)
+      var sox = new Sox(ws)
+      sox._onMessage(data) // deliver first message by hand
+
+      // wait for position message
+      sox.once('position', () => {
+        setTimeout(() => {
+          resolve(sox)
+        })
+      })
+    }
+    ws.on('message', message)
+  })
+}
+
+module.exports = connect
+
