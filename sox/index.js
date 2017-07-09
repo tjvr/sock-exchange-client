264627a4aa6c442cc5d914a97133fad72dfe4bbe
tjvr <tim@tjvr.org>
2017-07-09 16:59:23 +0100

    log while connecting

--- sox/index.js
+++ sox/index.js
@@ -4,7 +4,11 @@ const sox = require('./core')
 const connect = sox(WebSocket)
 
 function connectAs(host, name, token, cb) {
-  return connect(host, sox => sox.identify(name, token, cb))
+  console.log(`connecting to ${host}...`)
+  return connect(host, sox => {
+    console.log(`identifying as {name}...`)
+    sox.identify(name, token, cb)
+  })
 }
 
 module.exports = {
